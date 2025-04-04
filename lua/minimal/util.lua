ffd5937cbd4b6e9493203f09cf93d49b72f46a76
Marcos Bião <dev.marcosdly@gmail.com>
2025-04-04 09:29:58 -0300

    feat: define theme variants as standalone modules

--- lua/minimal/util.lua
+++ lua/minimal/util.lua
@@ -11,4 +11,17 @@ function M.load_theme_table(theme)
         end)
 end
 
+---Trivial utility function to make intent explicit.
+function M.patch_theme_table_values(original_theme, values_to_patch)
+    return vim.tbl_deep_extend('force', original_theme, values_to_patch)
+end
+
+---Factory of setup functions, allowing less copy-pasted boilerplate for variants
+function M.make_setup_function(original_theme)
+    return function (user_theme_patch)
+        local theme = M.patch_theme_table_values(original_theme, user_theme_patch or {})
+        M.load_theme_table(theme)
+    end
+end
+
 return M
