c17c57707e51bb36d9944ef71fb96e280244d864
Marcos Bião <dev.marcosdly@gmail.com>
2025-04-04 08:44:03 -0300

    refactor: rewrite theme loading utilities

--- lua/minimal/util.lua
+++ lua/minimal/util.lua
@@ -1,23 +1,14 @@
 local M = {}
 
- local function highlight(group, properties)
-   local bg = properties.bg == nil and '' or 'guibg=' .. properties.bg
-   local fg = properties.fg == nil and '' or 'guifg=' .. properties.fg
-   local style = properties.style == nil and '' or 'gui=' .. properties.style
-   local cmd = table.concat({ 'highlight', group, bg, fg, style }, ' ')
-   vim.api.nvim_command(cmd)
- end
+function M.load_theme_table(theme)
+    vim.cmd.highlight "clear"
+    return vim.iter(theme)
+        :map(function(_, highlight_table)
+            return vim.tbl_values(highlight_table)
+        end)
+        :each(function(group, color_props)
+            vim.api.nvim_set_hl(0, group, color_props)
+        end)
+end
 
- function M.load(theme)
-   if vim.g.colors_name then
-     vim.cmd 'hi clear'
-   end
-   vim.o.termguicolors = true
-   for _, v in pairs(theme) do
-     for group, properties in pairs(v) do
-       highlight(group, properties)
-     end
-   end
- end
-
- return M
+return M
