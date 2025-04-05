0890dd578ab7084ae67587dae6d9c793afe2b76e
Marcos Bião <dev.marcosdly@gmail.com>
2025-04-05 07:08:56 -0300

    fix(util): expected table, got string; useless callback spaghetti

--- lua/minimal/util.lua
+++ lua/minimal/util.lua
@@ -2,23 +2,21 @@ local M = {}
 
 function M.load_theme_table(theme)
     vim.cmd.highlight "clear"
-    return vim.iter(theme)
-        :map(function(_, highlight_table)
-            return vim.tbl_values(highlight_table)
-        end)
-        :each(function(group, color_props)
+    for _, theme_section in pairs(theme) do
+        for group, color_props in pairs(theme_section) do
             vim.api.nvim_set_hl(0, group, color_props)
-        end)
+        end
+    end
 end
 
 ---Trivial utility function to make intent explicit.
 function M.patch_theme_table_values(original_theme, values_to_patch)
-    return vim.tbl_deep_extend('force', original_theme, values_to_patch)
+    return vim.tbl_deep_extend("force", original_theme, values_to_patch)
 end
 
 ---Factory of setup functions, allowing less copy-pasted boilerplate for variants
 function M.make_setup_function(original_theme)
-    return function (user_theme_patch)
+    return function(user_theme_patch)
         local theme = M.patch_theme_table_values(original_theme, user_theme_patch or {})
         M.load_theme_table(theme)
     end
